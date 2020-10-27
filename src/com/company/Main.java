49daa32208492a6d3a1a17c856bae0fb2633be3d
Baturay <mehmetbaturayyasar@gmail.com>
2020-10-27 23:16:55 +0100

    FirstCommit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the value of first number:");
+        double a = scan.nextInt();
+
+        System.out.println("Enter the value of second number:");
+        double b = scan.nextInt();
+
+        double answer;
+        double zero = a + b;
+        answer = ((a*a)+b) / ((a+b)*(a+b));
+        if (zero == 0){
+            System.out.println("Attempt to divide by zero");
+        }
+        else
+            System.out.println("The value of the expression: " + answer);
+
+
+
+    }
+}
